fae68f71741d3ba2c10154024892ea81be8875da
Michael Mburu <ichangaimichael@yahoo.com>
2023-07-16 13:17:46 +0300

    - Added google console head tag for verification

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '@/components/MDX'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
@@ -12,7 +13,12 @@ function MyApp({ Component, pageProps }) {
       <MDXProvider components={MDXComponents}>
         {getLayout(<Component {...pageProps} />)}
       </MDXProvider>
-      <meta name="google-site-verification" content="xOxDON0o9yfmcqSLaMqQluMdJNAR_Zb8v6aZ26aCYCc" />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="xOxDON0o9yfmcqSLaMqQluMdJNAR_Zb8v6aZ26aCYCc"
+        />
+      </Head>
     </>
   )
 }
